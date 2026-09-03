0962d6f8822cbffcf19cc9cdd5adcd33924b6c6f
therooler <roel.wier@gmail.com>
2026-09-03 13:13:34 -0700

    fix import

--- src/geope/geometry/lie/__init__.py
+++ src/geope/geometry/lie/__init__.py
@@ -1,2 +1,2 @@
-from .basis import Basis, traces
+from .basis import Basis, traces, get_project_omegas_fn, get_project_omegas_fn_otf
 from .groups import MatrixLieGroup, SpecialUnitaryGroup, UnitaryGroup
